62731ca9d065df5a94b9a7e52ff355e62f37204e
Krzysztof Przystas <xainoofficial@gmail.com>
2025-09-29 12:58:41 +0200

    memek

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the root directory
+app.use(express.static(__dirname));
+
+// Endpoint to get a random meme image filename
+app.get('/random-meme', (req, res) => {
+    const memeCount = 5;
+    const randomIndex = Math.floor(Math.random() * memeCount) + 1;
+    const memeFile = `memek${randomIndex}.jpg`;
+    res.json({ meme: memeFile });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
