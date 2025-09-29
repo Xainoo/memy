eb2755134ec6163d225c8fed8706bf85b1e7b545
Krzysztof Przystas <xainoofficial@gmail.com>
2025-09-29 13:07:59 +0200

    terminal message update

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.get('/random-meme', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}/public/index.html`);
 });
